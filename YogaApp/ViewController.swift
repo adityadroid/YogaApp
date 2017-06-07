d5471ad1287c0e5b0150f576aef4063be53d5fb9
Aditya Gurjar <adityaadi1467@gmail.com>
2017-06-07 15:30:41 +0530

    Initial Commit

---
+++ YogaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YogaApp
+//
+//  Created by Aditya Gurjar on 6/7/17.
+//  Copyright © 2017 Aditya Gurjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
